8d31d49e37ef1f26cca06da5476c2d5a42ad4fb9
Jesús <Jess@DESKTOP-LS8LHC6>
2024-10-30 18:00:50 +0100

    añado la tarea replicator

---
+++ src/tareas/Replicator.java
@@ -0,0 +1,14 @@
+package tareas;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Jesús
+ */
+public class Replicator {
+    
+}
