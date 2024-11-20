075bc6eed62fe3c93b135134905af8358b60a651
elkin <elkin@DESKTOP-52AJPDN>
2024-11-20 17:43:14 +0100

    Agregator and Enrincher

--- src/tasks/Agregator.java
+++ src/tasks/Agregator.java
@@ -1,15 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package tasks;
 
+import cafe.Slot;
 import cafe.Task;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+/**
+ *
+ * @author elkin
+ */
 public class Agregator implements Task {
 
-    public Agregator() {
+    private Slot entrySlot, exitSlot;
+
+    public Slot getEntrySlot() {
+        return entrySlot;
     }
 
-    @Override
-    public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void setEntrySlot(Slot entrySlot) {
+        this.entrySlot = entrySlot;
     }
-    
+
+    public Slot getExitSlot() {
+        return exitSlot;
+    }
+
+    public void setExitSlot(Slot exitSlot) {
+        this.exitSlot = exitSlot;
+    }
+
+    public Agregator() {
+
+    }
+
+    public void run(XPathExpression exp, XPathExpression exp2) {
+        try {
+
+            NodeList nl = (NodeList) exp.evaluate(entrySlot.getBuffer(), XPathConstants.NODESET);
+
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = dbFactory.newDocumentBuilder();
+            int tamaño = entrySlot.bufferSize();
+
+            Document aux = builder.newDocument();
+
+            //se crea un elemento raiz cafe_order en el nuevo documento y se agrega al documento
+            Element rootElement = aux.createElement("cafe_order");
+            aux.appendChild(rootElement);
+
+            //se crea un elemento order_id y se le asigna un valor obtenido del primer nodo de la lista
+            Element id = aux.createElement("order_id");
+            id.appendChild(aux.createTextNode(nl.item(0).getChildNodes().item(0).getTextContent()));
+            rootElement.appendChild(id);
+
+            //Se crea un elemento drinks que contendra las bebidas que se agregen más tarde
+            Element drs = aux.createElement("drinks");
+            rootElement.appendChild(drs);
+
+            for (int i = 0; i < tamaño; i++) {
+
+                NodeList nl2 = (NodeList) exp2.evaluate(entrySlot.getBuffer(), XPathConstants.NODESET);
+
+                Node drink = nl2.item(0);
+
+                aux.adoptNode(drink);
+                drs.appendChild(drink);
+
+                entrySlot.next();
+
+            }
+            exitSlot.receiveData(aux);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- src/tasks/ContentEnrincher.java
+++ src/tasks/ContentEnrincher.java
@@ -1,16 +1,86 @@
 package tasks;
 
+import cafe.Slot;
 import cafe.Task;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 
 public class ContentEnrincher implements Task {
 
+    private Slot EntrySlot1;
+    private Slot EntrySlot2;
+    private Slot ExitSlot;
+
+    public Slot getEntrySlot1() {
+        return EntrySlot1;
+    }
+
+    public void setEntrySlot1(Slot entrySlot) {
+        this.EntrySlot1 = entrySlot;
+    }
+    
+    public Slot getEntrySlot2() {
+        return EntrySlot2;
+    }
+
+    public void setEntrySlot2(Slot entrySlot) {
+        this.EntrySlot2 = entrySlot;
+    }
+
+    public Slot getExitSlot() {
+        return ExitSlot;
+    }
+
+    public void setExitSlot(Slot ExitSlot) {
+        this.ExitSlot = ExitSlot;
+    }
     public ContentEnrincher() {
     }
 
-    @Override
-    public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void run(XPathExpression exp, XPathExpression exp2, XPathExpression exp3) {
+        try {
+
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = dbFactory.newDocumentBuilder();
+            int tamaño = EntrySlot1.bufferSize();
+            for (int i = 0; i < tamaño; i++) {
+                Document aux = builder.newDocument();
+
+                Element rootElement = aux.createElement("mensaje");
+                aux.appendChild(rootElement);
+
+                NodeList nl = (NodeList) exp.evaluate(EntrySlot1.getBuffer(), XPathConstants.NODESET);
+                NodeList nl2 = (NodeList) exp2.evaluate(EntrySlot1.getBuffer(), XPathConstants.NODESET);
+                NodeList nl3 = (NodeList) exp3.evaluate(EntrySlot2.getBuffer(), XPathConstants.NODESET);
+
+                Node cabecera = nl.item(0);
+                Node drink = nl2.item(0);
+                Node stock = nl3.item(0);
+
+                aux.adoptNode(cabecera);
+                rootElement.appendChild(cabecera);
+
+                aux.adoptNode(drink);
+                rootElement.appendChild(drink);
+
+                aux.adoptNode(stock);
+                drink.appendChild(stock);
+
+                ExitSlot.receiveData(aux);
+                EntrySlot1.next();
+                EntrySlot2.next();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
 }
